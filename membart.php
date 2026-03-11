dbdd999321625e21929eec312c95dd3e95117898
renzxwilliams-boop <renzxwilliams@gmail.com>
2026-03-11 20:47:33 +0800

    Add files via upload

---
+++ membart.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Creating Stored Procedures</title>
+</head>
+<body>
+
+<h1>Task 2: Creating Views </h1>
+
+<h2>View 1 - Salary</h2>
+<img src="ss1.png" width="300">
+
+<h2>View 2 - Total Salary</h2>
+<img src="ss2.png" width="300">
+
+<h2>View 3 - Location</h2>
+<img src="ss3.png" width="300">
+
+</body>
+</html>
